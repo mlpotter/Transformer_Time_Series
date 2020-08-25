64a0f56fb26c2f730cc9afcb2e91e5ac87082b7d
Michael Potter <35242331+mlpotter@users.noreply.github.com>
2020-08-25 08:52:04 -0700

    Add files via upload

---
+++ Dataloader.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class time_series_paper(Dataset):
+    """synthetic time series dataset from section 5.1"""
+    
+    def __init__(self,t0=96,N=4500,transform=None):
+        """
+        Args:
+            t0: previous t0 data points to predict from
+            N: number of data points
+            transform: any transformations to be applied to time series
+        """
+        self.t0 = t0
+        self.N = N
+        self.transform = None
+        
+        # time points
+        self.x = torch.cat(N*[torch.arange(0,t0+24).type(torch.float).unsqueeze(0)])
+
+        # sinuisoidal signal
+        A1,A2,A3 = 60 * torch.rand(3,N)
+        A4 = torch.max(A1,A2)        
+        self.fx = torch.cat([A1.unsqueeze(1)*torch.sin(np.pi*self.x[0,0:12]/6)+72 ,
+                        A2.unsqueeze(1)*torch.sin(np.pi*self.x[0,12:24]/6)+72 ,
+                        A3.unsqueeze(1)*torch.sin(np.pi*self.x[0,24:t0]/6)+72,
+                        A4.unsqueeze(1)*torch.sin(np.pi*self.x[0,t0:t0+24]/12)+72],1)
+        
+        self.masks = self._generate_square_subsequent_mask(24)
+                
+        
+        # print out shapes to confirm desired output
+        print("x: {}*{}".format(*list(self.x.shape)),
+              "fx: {}*{}".format(*list(self.fx.shape)))        
+        
+    def __len__(self):
+        return len(self.fx)
+    
+    def __getitem__(self,idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+            
+        sample = {"x":self.x[idx,0:t0],
+                  "x_next":self.x[idx,t0:t0+24],
+                  "fx": self.fx[idx,0:t0],
+                  "fx_next":self.fx[idx,t0:t0+24],
+                  "attention_masks":self.masks}
+        
+        if self.transform:
+            sample=self.transform(sample)
+            
+        return sample
+    
+    def _generate_square_subsequent_mask(self, sz):
+        mask = (torch.tril(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float(0.0)).masked_fill(mask == 1, float('-inf'))
+        return mask
---
+++ causal_convolution_layer.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+
+class CausalConv1d(torch.nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=0,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+        
+        self.__padding = (kernel_size - 1) * dilation
+        
+    def forward(self, input):
+        return super(CausalConv1d, self).forward(F.pad(input, (self.__padding, 0)))
+
+
+class context_embedding(torch.nn.Module):
+    def __init__(self,in_channels=1,embedding_size=256,k=5):
+        super(context_embedding,self).__init__()
+        self.causal_convolution = CausalConv1d(in_channels,embedding_size,kernel_size=k)
+
+    def forward(self,x):
+        x = self.causal_convolution(x)
+        return F.tanh(x)
+
